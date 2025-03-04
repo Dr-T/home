f581098d9e1e0877c216d6f5deeb91427415c823
Dr-T <xandertang@qq.com>
2025-03-04 20:30:57 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,18 +11,18 @@
             -
           </span>
           {{ fullYear }}
-          <a :href="siteUrl">{{ siteAuthor }}</a>
+//           <a :href="siteUrl">{{ siteAuthor }}</a>
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
+          &amp;&nbsp;Powered&nbsp;by
+//           <a :href="config.github" target="_blank">
             {{ config.author }}
           </a>
         </span>
         <!-- 站点备案 -->
         <span>
-          &amp;
+//          &amp;
           <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
             {{ siteIcp }}
           </a>
